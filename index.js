af931e7848a1f3c9724c6bb513608158a602bfff
vishnu <akshaygupta7699181@gmail.com>
2026-05-16 10:32:47 +0530

    fixed commit

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+
 const port = 8080;
 
-app.set("view engine" , "ejs");
+app.use(express.urlencoded({ extended: true }));
 
-app.set("views" , path_join(__dirname,"views"));
-app.set(express.static(path_join(__dirname,"public")));
+app.set("view engine", "ejs");
 
-app.use(express.urlencoded({extended : true}));
+app.set("views", path.join(__dirname, "views"));
 
+app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(port , () => {
-    console.log(`port is lisening on ${port}`);
-})
+let data1 = [
+    {
+        username: "vishnu gupta",
+        thougth: "i have to best version of myself"
+    },
+    {
+        username: "vaibhav gupta",
+        thougth: "nind nhii aati h mjhe when i work"
+    },
+    {
+        username: "akshay gupta",
+        thougth: "chije thk rkho bhau appne app sb thk hoga"
+    },
+];
+
+app.get("/posts", (req, res) => {
+    res.render("index.ejs", { data1 });
+});
+
+app.listen(port, () => {
+    console.log(`port is listening on ${port}`);
+});
